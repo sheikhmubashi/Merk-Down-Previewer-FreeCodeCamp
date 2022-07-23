e34ba353ad6fd25a64bcf7f59e821c7209de1a3b
Sheikh Mubashir <smubashir655@gmail.com>
2022-07-23 18:54:37 +0500

    second free code camp project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+  marked.setOptions({
+    breaks: true
+  })
+  const [text, setText] = useState(`
+  # This is H1
+  ## This is H2
+  [This is a Title](https://www.example.com)
+  \`This one is a code block\`
+  \`\`\`
+  {
+    "firstName": "Mubashir",
+    "lastName": "Ejaz",
+    "age": 24k
+  }
+  \`\`\`
+  - I'm a good learner
+- I'm a self taught developer
+- I love javaScript
+
+> Every great developer you know got there by solving problems they were unqualified to solve until they actually did it. 
+
+![My image]({image})
+
+**bold text**
+  `)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id='editor' onChange={(e) => { setText(e.target.value) }} value={text}></textarea>
+      <div
+        id='preview'
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}>
+      </div>
     </div>
   );
 }
